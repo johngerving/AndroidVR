078410704ab67e1d07a6c97101675be3853603d6
johngerving <gervingjohn@gmail.com>
2020-11-09 14:12:54 -0800

    Added some quaternion code

--- app/src/main/java/com/ggamer/androidvr/MainActivity.java
+++ app/src/main/java/com/ggamer/androidvr/MainActivity.java
@@ -28,10 +28,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private float[] mAccelerometerData = new float[3];
     private float[] mMagnetometerData = new float[3];
     private float[] mGyroscopeData = new float[3];
+    private float[] mPreviousGyroscopeData = new float[3];
 
-    private float fusedAzimuth = 0.0f;
-    private float fusedPitch = 0.0f;
-    private float fusedRoll = 0.0f;
+    private Quaternion fusedQuaternion = new Quaternion(.0f, 0.0f, 0.0f, 0.0f);
+    private Quaternion previousFusedQuaternion = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+
+    private Quaternion invFusedQuaternion = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+
+    private Quaternion deltaQ;
+
+    private boolean isOrientationInitialized = false;
+
+    private Vector3 estimatedGravity;
+    private Vector3 measuredGravity;
 
     private float dt = 0.0f;
 
@@ -87,11 +96,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+
         int sensorType = event.sensor.getType();
 
         switch (sensorType) {
             case Sensor.TYPE_ACCELEROMETER:
                 mAccelerometerData = event.values.clone();
+                measuredGravity = new Vector3(mAccelerometerData[0], mAccelerometerData[1], mAccelerometerData[2]);
+                measuredGravity.normalize();
                 break;
             case Sensor.TYPE_MAGNETIC_FIELD:
                 mMagnetometerData = event.values.clone();
@@ -104,99 +116,66 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
 
         float[] rotationMatrix = new float[9];
-        boolean rotationOK = SensorManager.getRotationMatrix(rotationMatrix, null, mAccelerometerData, mMagnetometerData);
+        SensorManager.getRotationMatrix(rotationMatrix, null, mAccelerometerData, mMagnetometerData);
 
-        float orientationValues[] = new float[3];
-        if(rotationOK) {
-            SensorManager.getOrientation(rotationMatrix, orientationValues);
-        }
 
-        float accAzimuth = orientationValues[0];
-        float accPitch = orientationValues[1];
-        float accRoll = orientationValues[2];
+        Quaternion accQuaternion;
+        accQuaternion = rotationMatrixToQuaternion(rotationMatrix);
 
-        // This timestep's delta rotation to be multiplied by the current rotation
-        // after computing it from the gyro sample data.
-//        if (timestamp != 0) {
-//            final float dT = (event.timestamp - timestamp) * NS2S;
-//            // Axis of the rotation sample, not normalized yet.
-//            float axisX = mGyroscopeData[0];
-//            float axisY = mGyroscopeData[0];
-//            float axisZ = mGyroscopeData[0];
-//
-//            // Calculate the angular speed of the sample
-//            float omegaMagnitude = (float) Math.sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
-//
-//            // Normalize the rotation vector if it's big enough to get the axis
-//            // (that is, EPSILON should represent your maximum allowable margin of error)
-//            if (omegaMagnitude > EPSILON) {
-//                axisX /= omegaMagnitude;
-//                axisY /= omegaMagnitude;
-//                axisZ /= omegaMagnitude;
-//            }
-//
-//            // Integrate around this axis with the angular speed by the timestep
-//            // in order to get a delta rotation from this sample over the timestep
-//            // We will convert this axis-angle representation of the delta rotation
-//            // into a quaternion before turning it into the rotation matrix.
-//            float thetaOverTwo = omegaMagnitude * dT / 2.0f;
-//            float sinThetaOverTwo = (float) Math.sin(thetaOverTwo);
-//            float cosThetaOverTwo = (float) Math.cos(thetaOverTwo);
-//            deltaRotationVector[0] = sinThetaOverTwo * axisX;
-//            deltaRotationVector[1] = sinThetaOverTwo * axisY;
-//            deltaRotationVector[2] = sinThetaOverTwo * axisZ;
-//            deltaRotationVector[3] = cosThetaOverTwo;
-//        }
-//        timestamp = event.timestamp;
-//        float[] deltaRotationMatrix = new float[9];
-//        SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
-//
-//        float[] gyroscopeOrientationVector = new float[3];
-//
-//        gyroscopeOrientationVector[0] = (float) (Math.cos(gyroscopeOrientation[0]) * Math.cos(gyroscopeOrientation[1]));
-//        gyroscopeOrientationVector[1] = (float) (Math.sin(gyroscopeOrientation[0]) * Math.cos(gyroscopeOrientation[1]));
-//        gyroscopeOrientationVector[2] = (float) (Math.sin(gyroscopeOrientation[1]));
-//
-//        float[] newGyroscopeOrientationVector = new float[3];
-//
-//        newGyroscopeOrientationVector = multiplyVectorByMatrix(gyroscopeOrientationVector, deltaRotationMatrix);
-//        SensorManager.getRotationMatrixFromVector(newGyroscopeOrientationVector, deltaRotationMatrix);
-//        SensorManager.getOrientation(deltaRotationMatrix, gyroscopeOrientation);
-        // User code should concatenate the delta rotation we computed with the current rotation
-        // in order to get the updated rotation.
-        // rotationCurrent = rotationCurrent * deltaRotationMatrix;
+        Quaternion gyroQuaternion = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+
 
         if(timestamp != 0) {
             dt = (event.timestamp - timestamp) * NS2S;
-            if(fusedAzimuth == 0.0f && fusedPitch == 0.0f && fusedRoll == 0.0f) {
-                fusedAzimuth = accAzimuth;
-                fusedPitch = accPitch;
-                fusedRoll = accRoll;
-            } else {
-                fusedAzimuth += Math.toDegrees(mGyroscopeData[0]) * dt;
-                fusedPitch += Math.toDegrees(mGyroscopeData[1]) * dt;
-                fusedRoll += Math.toDegrees(mGyroscopeData[2]) * dt;
+            if(!isOrientationInitialized) {
+                previousFusedQuaternion = accQuaternion;
+                isOrientationInitialized = true;
             }
+            Quaternion gyroDeltaQ = gyroToQuaternionDelta(mGyroscopeData, dt);
 
-            int forceMagnitudeApprox = Math.round(Math.abs(accAzimuth) + Math.abs(accPitch) + Math.abs(accRoll));
-            if(forceMagnitudeApprox > 8192 && forceMagnitudeApprox < 32768) {
+            fusedQuaternion = new Quaternion(previousFusedQuaternion);
+            fusedQuaternion.multiply(gyroDeltaQ);
 
+            invFusedQuaternion = new Quaternion(fusedQuaternion);
+            invFusedQuaternion.inverse();
 
-                accPitch = (float) (Math.atan2(accPitch, accRoll) * 180 / Math.PI);
-                fusedPitch = (float) (fusedPitch * 0.98 + accPitch * 0.02);
+            estimatedGravity = new Vector3(0, 0, -1);
+            estimatedGravity.applyQuaternion(invFusedQuaternion);
+            estimatedGravity.normalize();
 
-                accRoll = (float) (Math.atan2(accAzimuth, accRoll) * 180 / Math.PI);
-                fusedRoll = (float) (fusedRoll * 0.98 + accRoll * 0.02);
+            if(measuredGravity == null) {
+                measuredGravity = new Vector3(0, 0, -1);
             }
+            deltaQ = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+            deltaQ.setFromUnitVectors(estimatedGravity, measuredGravity);
+            deltaQ.inverse();
+
+            Quaternion targetQ = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+            targetQ = new Quaternion(fusedQuaternion);
+            targetQ.multiply(deltaQ);
+
+
+
+            fusedQuaternion.slerp(targetQ, 1.0f - 0.98f);
+
+            previousFusedQuaternion = new Quaternion(fusedQuaternion);
+
+            float[] orientationEuler = new float[3];
+            orientationEuler = quaternionToEuler(fusedQuaternion);
+
+
 
             mTextSensorAzimuth.setText(getResources().getString(
-                    R.string.value_format, fusedAzimuth));
+                    R.string.value_format, orientationEuler[0]));
             mTextSensorPitch.setText(getResources().getString(
-                    R.string.value_format, fusedPitch));
+                    R.string.value_format, orientationEuler[1]));
             mTextSensorRoll.setText(getResources().getString(
-                    R.string.value_format, fusedRoll));
+                    R.string.value_format, orientationEuler[2]));
         }
         timestamp = event.timestamp;
+        mGyroscopeData[0] = mPreviousGyroscopeData[0];
+        mGyroscopeData[1] = mPreviousGyroscopeData[1];
+        mGyroscopeData[2] = mPreviousGyroscopeData[2];
     }
 
     @Override
@@ -213,4 +192,93 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         return result;
     }
+
+    public Quaternion eulerToQuaternion(float yaw, float pitch, float roll) {
+        float cy = (float) Math.cos(yaw * 0.5);
+        float sy = (float) Math.sin(yaw * 0.5);
+        float cp = (float) Math.cos(pitch * 0.5);
+        float sp = (float) Math.sin(pitch * 0.5);
+        float cr = (float) Math.cos(roll * 0.5);
+        float sr = (float) Math.sin(roll * 0.5);
+
+        float[] q = new float[4];
+        q[0] = cr * cp * cy + sr * sp * sy;
+        q[1] = sr * cp * cy - cr * sp * sy;
+        q[2] = cr * sp * cy + sr * cp * sy;
+        q[3] = cr * cp * sy - sr * sp * cy;
+
+        Quaternion result = new Quaternion(q[0], q[1], q[2], q[3]);
+        return result;
+    }
+
+    public float[] quaternionToEuler(Quaternion q) {
+        float[] angles = new float[3];
+
+        double sinr_cosp = 2 * (q.getW() * q.getI() + q.getJ() * q.getK());
+        double cosr_cosp = 1 - 2 * (q.getI() * q.getI() + q.getJ() * q.getJ());
+        angles[2] = (float) Math.atan2(sinr_cosp, cosr_cosp);
+
+        // pitch (y-axis rotation)
+        double sinp = 2 * (q.getW() * q.getJ() - q.getK() * q.getI());
+        if (Math.abs(sinp) >= 1) {
+            angles[1] = (float) Math.copySign(Math.PI / 2, sinp); // use 90 degrees if out of range
+        } else {
+            angles[1] = (float) Math.asin(sinp);
+        }
+        // yaw (z-axis rotation)
+        double siny_cosp = 2 * (q.getW() * q.getK() + q.getI() * q.getJ());
+        double cosy_cosp = 1 - 2 * (q.getJ() * q.getJ() + q.getK() * q.getK());
+        angles[0] = (float) Math.atan2(siny_cosp, cosy_cosp);
+
+        return angles;
+    }
+
+    public Quaternion rotationMatrixToQuaternion(float[] m) {
+        float tr = m[0] + m[4] + m[8];
+
+        float qw;
+        float qx;
+        float qy;
+        float qz;
+
+        if(tr > 0) {
+            float S = (float) (Math.sqrt(tr + 1.0)) * 2;
+            qw = 0.25f * S;
+            qx = (m[7] - m[5]) / S;
+            qy = (m[2] - m[6]) / S;
+            qz = (m[3] - m[1]) / S;
+        } else if((m[0] > m[4]) && (m[0] > m[8])) {
+            float S = (float) (Math.sqrt(1.0 + m[0] - m[4] - m[8]) * 2);
+            qw = (m[7] - m[5]) / S;
+            qx = 0.25f * S;
+            qy = (m[1] + m[3]) / S;
+            qz = (m[2] + m[6]) / S;
+        } else if(m[4] > m[8]) {
+            float S = (float) (Math.sqrt(1.0 + m[4] - m[0] - m[8]) * 2);
+            qw = (m[2] - m[6]) / S;
+            qx = (m[1] + m[3]) / S;
+            qy = 0.25f * S;
+            qz = (m[5] + m[7]) / S;
+        } else {
+            float S = (float) (Math.sqrt(1.0 + m[8] - m[0] - m[4]) * 2);
+            qw = (m[3] - m[1]) / S;
+            qx = (m[2] + m[6]) / S;
+            qy = (m[5] + m[6]) / S;
+            qz = 0.25f * S;
+        }
+
+        Quaternion result = new Quaternion(qw, qx, qy, qz);
+        return result;
+    }
+
+    public Quaternion gyroToQuaternionDelta(float[] gyro, float dt) {
+        Quaternion quat = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+        Vector3 axis;
+        Vector3 gyroVector = new Vector3(gyro[0], gyro[1], gyro[2]);
+        axis = new Vector3(gyro[0], gyro[1], gyro[2]);
+        axis.normalize();
+        quat.setFromAxisAngle(axis, gyroVector.length() * dt);
+        return quat;
+    }
+
 }
